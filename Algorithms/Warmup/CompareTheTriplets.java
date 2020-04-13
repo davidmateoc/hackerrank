d7ad4766e005ce673b9b93bd937b70c7131271d7
David Mateo Carmona <david.mateo@free-now.com>
2020-04-13 22:58:41 +0200

    Solution for Compare the Triplets problem

--- Algorithms/Warmup/CompareTheTriplets.java
+++ Algorithms/Warmup/CompareTheTriplets.java
@@ -1,4 +1,31 @@
+/**
+ * @author https://github.com/davidmateoc
+ * @problem https://www.hackerrank.com/challenges/compare-the-triplets
+ */
 package Warmup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CompareTheTriplets {
+
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int aliceScore = 0;
+        int bobScore = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i)) {
+                aliceScore++;
+            } else if (a.get(i) < b.get(i)){
+                bobScore++;
+            }
+        }
+        return new ArrayList<>(Arrays.asList(aliceScore, bobScore));
+    }
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>(Arrays.asList(5, 6, 7));
+        List<Integer> b = new ArrayList<>(Arrays.asList(3, 6, 10));
+        List<Integer> winner = compareTriplets(a, b);
+        System.out.println(winner);
+    }
 }
