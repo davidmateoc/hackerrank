4cb19548ce8e945ba23cbb96d707dd39fc8d6b2c
David Mateo Carmona <david.mateo@free-now.com>
2020-04-13 23:09:38 +0200

    Solution for A Very Big Sum problem

---
+++ Algorithms/Warmup/AVeryBigSum.java
@@ -0,0 +1,20 @@
+/**
+ * @author https://github.com/davidmateoc
+ * @problem https://www.hackerrank.com/challenges/a-very-big-sum
+ */
+package Warmup;
+
+public class AVeryBigSum {
+    static long aVeryBigSum(long[] ar) {
+        long sum = 0;
+        for (long i : ar) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        long sum = aVeryBigSum(new long[]{1, 2, 3});
+        System.out.println(sum);
+    }
+}
