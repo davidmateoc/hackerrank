52ec6147075735400114d8ca9c965f31c3633bf7
David Mateo Carmona <david.mateo@free-now.com>
2020-04-14 20:54:57 +0200

    Solution for Plus Minus problem

---
+++ Algorithms/Warmup/PlusMinus.java
@@ -0,0 +1,24 @@
+/**
+ * @author https://github.com/davidmateoc
+ * @problem https://www.hackerrank.com/challenges/diagonal-difference
+ */
+public class PlusMinus {
+    static void plusMinus(int[] arr) {
+        float denominator = arr.length;
+        float positive = 0;
+        float negative = 0;
+        float zeros = 0;
+        for(int i : arr) {
+            if (i > 0) {
+                positive++;
+            } else if (i < 0) {
+                negative++;
+            } else {
+                zeros++;
+            }
+        }
+        System.out.println(positive/denominator);
+        System.out.println(negative/denominator);
+        System.out.println(zeros/denominator);
+    }
+}
